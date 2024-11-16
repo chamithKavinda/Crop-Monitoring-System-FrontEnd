8046cfb000a0eb64c80890c4ff361b2278a4c7e4
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-17 00:19:11 +0530

    implement Function to show and close registration form

---
+++ controller/Staff.js
@@ -0,0 +1,27 @@
+// Function to show and close registration form
+document.addEventListener('DOMContentLoaded', () => {
+    const addStaffButton = document.getElementById('add-staff');
+    const staffRegisterForm = document.getElementById('staffRegisterForm');
+    const closeButton = document.getElementById('staffRegisterForm-close');
+
+    const openForm = () => {
+        staffRegisterForm.style.display = 'flex'; 
+    };
+
+    const closeForm = () => {
+        staffRegisterForm.style.display = 'none'; 
+    };
+
+    addStaffButton.addEventListener('click', openForm);
+
+    closeButton.addEventListener('click', closeForm);
+
+    window.addEventListener('click', (event) => {
+        if (event.target === staffRegisterForm) {
+            closeForm();
+        }
+    });
+
+    closeForm();
+});
+
